c70f64a111a67efc6a2aba700c4425fc96971b73
Spoon <Jimmy-Patrick-Coleman@outlook.com>
2021-05-02 15:30:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+
+function operate(numberOne, numberTwo, operator) {
+    if (operator === '+') {
+        return add(numberOne, numberTwo)
+    } else if (operator === 'subtract') {
+        return subtract(numberOne, numberTwo)
+    } else if (operator === 'multiply') {
+        return multiply(numberOne, numberTwo)
+    } else if (operator === 'divide') {
+        return divide(numberOne, numberTwo)
+    }
+}
+
+function calculate(a, b) {
+    let o = prompt('enter operator', '+')
+    let answer = operate(a, b, o);
+return answer
+}
+console.log(calculate(8, 5))
