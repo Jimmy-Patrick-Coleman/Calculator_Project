36c47aaa634f1841e1ce61dee5a7427d2e8e25cf
Spoon <Jimmy-Patrick-Coleman@outlook.com>
2021-05-02 16:36:45 +0100

    buttons display numbers now

--- script.js
+++ script.js
@@ -24,10 +24,56 @@ function operate(numberOne, numberTwo, operator) {
     }
 }
 
-/* keep this just for now */
+/* 
+
+keep this just for now 
+
 function calculate(a, b) {
     let o = prompt('enter operator', '+')
     let answer = operate(a, b, o);
 return answer
 }
 console.log(calculate(8, 5))
+
+*/
+
+/*starting use dom after this will save now */
+
+/* display scetion */
+const display = document.querySelector('#display');
+
+const displayValue = document.createElement('div');
+displayValue.classList.add('displayValue');
+displayValue.textContent = 'Enter a Number';
+
+
+let displayTextArray = [];
+
+display.appendChild(displayValue);
+/* end of display section */
+
+
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+      if (button.id === '1' || button.id === '2' || button.id === '3' || button.id === '4'
+      || button.id === '5'|| button.id === '6'|| button.id === '7'|| button.id === '8'
+      || button.id === '9'|| button.id === '0'|| button.id === '.'
+      ) {
+        displayTextArray.push(button.id)
+        changeDisplay(displayTextArray)
+      }
+  
+  });
+});
+
+/*array text function */
+function changeDisplay(array) {
+let newArray = array.join('')
+return displayValue.textContent = newArray;
+}
+/* */
